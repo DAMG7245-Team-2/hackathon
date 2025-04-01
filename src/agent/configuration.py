b8c39e8917e77ac5853d450646fd19f4c728af38
Ashish Nevan <nevchris242@gmail.com>
2025-03-31 23:55:54 -0400

    interviewprep-agent

--- src/agent/configuration.py
+++ src/agent/configuration.py
@@ -7,16 +7,39 @@ from typing import Optional
 
 from langchain_core.runnables import RunnableConfig
 
+MY_REPORT_STRUCTURE = """Use the following structure to generate a report on user provided job description:
+
+1. Introduction (no research needed)
+    - Highlight top technical skills, tools and technologies needed for the job
+    - Structured table with 3 columns: Technical skill, Description, Importance (Required, Preferred)
+
+2. Roadmap (no research needed)
+    - mermaid markdown code for a flowchart diagram of the recommended roadmap to achieve the preparation for the topic divided into 3 sections (Fundamentals, Intermediate, Advanced)
+    - Each node in the diagram should have a name of the concept
+
+3. Main Body Sections (research needed)
+    - Each section should focus on a concept that is relevant to the job description
+    - Provide at least 1 structured element (either a list or a table) per section that is relevant to the concept/skill discussed in the section
+    - Include at least 3 multiple choice questions per section that tests the understanding of the concept/skill discussed in the section
+
+4. Conclusion (no research needed)
+    - Aim for 1 structural element that distills the main concepts covered in the main body sections
+    - Provide a concise summary of the report"""
+
 
 @dataclass(kw_only=True)
 class Configuration:
     """The configuration for the agent."""
 
-    # Changeme: Add configurable values here!
-    # these values can be pre-set when you
-    # create assistants (https://langchain-ai.github.io/langgraph/cloud/how-tos/configuration_cloud/)
-    # and when you invoke the graph
-    my_configurable_param: str = "changeme"
+    report_structure: str = MY_REPORT_STRUCTURE
+    planner_provider: str = "openai"
+    planner_model: str = "o3-mini"
+    writer_provider: str = "openai"
+    writer_model: str = "gpt-4o-mini"
+    search_api: str = "tavily"
+    number_of_queries: int = 2
+    top_k: int = 2 # pinecone top k results
+    max_search_depth: int = 2
 
     @classmethod
     def from_runnable_config(
--- src/agent/state.py
+++ src/agent/state.py
@@ -1,17 +1,68 @@
 """Define the state structures for the agent."""
 
 from __future__ import annotations
+from typing import Dict, List, Literal, TypedDict, Annotated
+from pydantic import BaseModel, Field
+import operator
 
-from dataclasses import dataclass
+
+class Section(BaseModel):
+    name: str = Field(description="Name of this section in the report")
+    description: str = Field(
+        description="Brief description of the concept/skill covered in this section"
+    )
+    research: bool = Field(
+        description="Whether to perform research for this section of the report."
+    )
+    content: str = Field(description="Content of this section")
 
 
-@dataclass
-class State:
-    """Defines the input state for the agent, representing a narrower interface to the outside world.
+class Sections(BaseModel):
+    sections: List[Section] = Field(description="List of sections in the report")
 
-    This class is used to define the initial state and structure of incoming data.
-    See: https://langchain-ai.github.io/langgraph/concepts/low_level/#state
-    for more information.
-    """
 
-    changeme: str = "example"
+class SearchQuery(BaseModel):
+    search_query: str = Field(description="Query for web search")
+
+
+class Queries(BaseModel):
+    queries: List[SearchQuery] = Field(description="List of search queries")
+
+
+class ReportStateInput(TypedDict):
+    topic: str
+
+
+class ReportStateOutput(TypedDict):
+    final_report: str
+
+
+class ReportState(TypedDict):
+    topic: str
+    sections: List[Section]
+    completed_sections: Annotated[list, operator.add]
+    report_sections_from_research: str
+    final_report: str
+
+
+class SectionState(TypedDict):
+    topic: str
+    section: Section
+    search_iterations: int
+    search_queries: list[SearchQuery]
+    source_str: str
+    report_sections_from_research: str
+    completed_sections: list[Section]
+
+
+class SectionOutputState(TypedDict):
+    completed_sections: list[Section]
+
+
+class Feedback(BaseModel):
+    grade: Literal["pass", "fail"] = Field(
+        description="Evaluation result indicating whether the response meets requirements ('pass') or needs revision ('fail')."
+    )
+    follow_up_queries: list[SearchQuery] = Field(
+        description="Follow-up queries to refine the search for more information."
+    )
